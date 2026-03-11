79627ad4743350b0f07c9428b635b1b45ebc7ffa
bsucs <122378640+bsucs@users.noreply.github.com>
2026-03-11 13:47:33 -0400

    final version

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -11,13 +11,17 @@ onMounted(() => {
 
 <template> 
     <h1>Gallery Of the most reliable cars.</h1>
+
+
+     <h1>Toyota Lets go Places</h1>
     <div class="gallery">
+        <h2>{{ store.overview }}</h2>
         <div v-for="item in store.photos" :key="item.id" class="gallery-item">
             <img :src="item.src" :alt="item.alt" />
             <p>{{ item.alt }}</p>
         </div>
 
-       <h1>Toyota Lets go Places</h1>
+    
 
 
     </div>
